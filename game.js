2ce805373e1040d3dbe39238bb230b8b5d6f0798
Nikita_Rama <nikita.rama10@gmail.com>
2023-03-21 22:25:29 -0500

    Second Day

---
+++ game.js
@@ -0,0 +1,13 @@
+// const canvas = document.getElementById("tetris");
+// const ctx = canvas.getContext('2d');
+// const scale = 20;
+
+// ctx.scale(scale, scale);
+
+// const tWidth = canvas.width / scale;
+// const tHeight = canvas.height / scale;
+// ctx.fillStyle = "#000";
+// ctx.fillRect(0, 0, canvas.width, canvas.height);
+// ctx.scale(scale, scale);
+
+
